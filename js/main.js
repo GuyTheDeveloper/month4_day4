a7e2d957ff98db37a7555fdbe333ad4ad159a3e3
GuyTheDeveloper <goldenrodge99@gmail.com>
2022-05-04 20:39:22 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let mainArr = []; //dont ctrl c + ctrl v my code plz//
+const task1 = document.getElementById("age-table");
+const task2 = document.querySelectorAll("td > label");
+const task3 = document.querySelector("td");
+const task4 = document.querySelector("[name = search]");
+const task5 = document.querySelector("input");
+const task6 = document.querySelector("[type=submit]");
+
+mainArr.push(task1, task2, task3, task4, task5, task6);
+for (let i = 0; i < mainArr.length; i++) {
+  console.log(mainArr[i]);
+} //owner GuyTheDeveloper
